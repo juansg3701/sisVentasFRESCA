c046a1a87c7f30348dcb0126d46dbecdccf9e9c4
juansg3701 <54075068+juansg3701@users.noreply.github.com>
2020-10-14 00:23:50 -0500

    cambios cargos

--- resources/views/almacen/usuario/permiso/cargo/search.blade.php
+++ resources/views/almacen/usuario/permiso/cargo/search.blade.php
@@ -1,6 +1,5 @@
 {!! Form::open(array('url'=>'almacen/usuario/permiso/cargo','method'=>'GET','autocomplete'=>'off','role'=>'search')) !!}
 <div class="form-group">
-		<br>
 			<input id="car1"  type="text" class="form-control" name="searchText" placeholder="Buscar..." value="{{$searchText}}">
 		</br>
 
--- resources/views/layouts/layout3.blade.php
+++ resources/views/layouts/layout3.blade.php
@@ -85,8 +85,21 @@
         <nav class="navbar navbar-expand-sm navbar-default">
             <div id="main-menu" class="main-menu collapse navbar-collapse">
                 <ul class="nav navbar-nav">
+                    <li class="menu-item-has-children dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-cogs"></i>Permisos admin</a>
+                        <ul class="sub-menu children dropdown-menu">                        
+
+                            <li><i class="fa fa-id-badge"></i><a href="{{url('almacen/usuario/permiso/cargo')}}">Cargos</a>
+                            </li>
+                            <li><i class="fa fa-id-badge"></i><a href="{{url('almacen/usuario/permiso/usuario')}}">Permisos de usuarios</a>
+                            </li>
+                            <li><i class="fa fa-id-badge"></i><a href="{{url('almacen/usuario/permiso/cuenta')}}">Cuentas</a>
+                            </li>
+                         
+                        </ul>
+                    </li>
                     <li class="active">
-                        <a href="index.html"><i class="menu-icon fa fa-laptop"></i>Dashboard </a>
+                        <a href="{{url('almacen/usuario/registrar')}}"><i class="menu-icon fa fa-laptop"></i>Registrar Usuarios </a>
                     </li>
                     <li class="menu-title">UI elements</li><!-- /.menu-title -->
                     <li class="menu-item-has-children dropdown">
