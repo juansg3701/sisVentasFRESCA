5f587ebff56af8c071217f1da00131ee02533aaf
juansg3701 <54075068+juansg3701@users.noreply.github.com>
2020-10-19 23:18:12 -0500

    modales

--- resources/views/almacen/usuario/permiso/usuario/index.blade.php
+++ resources/views/almacen/usuario/permiso/usuario/index.blade.php
@@ -180,134 +180,13 @@
 		</div>
 	</div>
 
+	<a href="" data-target="#modal-registrar" data-toggle="modal"><button class="btn btn-outline-danger btn-sm">Registrar</button></a>
 
-	<!--Formulario de búsqueda y opciones-->
-	<div class="content">
-		<div class="animated fadeIn">
-			<div class="row">
-				<div class="col-md-12">
-					<div class="card">
-						<div class="card-header" align="center">
-							<h2 class="pb-2 display-5">REGISTRAR PERMISO</h2>
-						</div><br>
-						<div class="row" align="center">	
-							<div class="col-sm-3" align="center"></div>
-								<div class="col-sm-6" align="center">
-									<div class="card" align="center">
-										<div class="card-header" align="center">
-											<strong></strong>
-										</div>
-										<div class="card-body card-block" align="center">
-											<div id=formulario>
-												<div class="form-group">
-													<!--Incluir la ventana modal de búsqueda-->	
-													{!!Form::open(array('url'=>'almacen/usuario/permiso/usuario','method'=>'POST','autocomplete'=>'off'))!!}
-										    		{{Form::token()}}
+	<a href="" data-target="#modal-borrar" data-toggle="modal"><button class="btn btn-outline-danger btn-sm">Eliminar</button></a>
 
-										<div id=formulario align="center">
-													
-													<br>
-													Cargo:
-														<select name="id_cargo" class="form-control">
-													@foreach($cargos as $car)
-													<option name="id_cargo" value="{{$car->id_cargo}}">{{$car->nombre}}</option>
-													@endforeach
-												</select>
-										<br>
-												Módulo:
-													<select name="id_modulo" class="form-control">
-													@foreach($mod as $m)
-													<option name="id_modulo" value="{{$m->id_modulo}}">{{$m->nombre}}</option>
-													@endforeach
-												</select>
+	@include('almacen.usuario.permiso.usuario.modalRegistrar')
+	@include('almacen.usuario.permiso.usuario.modalBorrar')
 
-										<br>
-										<br>
-													<div align="center">
-													<button type="submit" class="btn btn-info">Asignar permiso</button>
-													<a href="{{url('/')}}" class="btn btn-danger">Volver</a>
-													</div>
-												</div>
-
-										{!!Form::close()!!}	
-												</div>
-											</div>
-										</div>
-									</div>
-								</div>
-							<div class="col-sm-3" align="center"></div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-
-
-	<!--Formulario de búsqueda y opciones-->
-	<div class="content">
-		<div class="animated fadeIn">
-			<div class="row">
-				<div class="col-md-12">
-					<div class="card">
-						<div class="card-header" align="center">
-							<h2 class="pb-2 display-5">ELIMINAR PERMISO</h2>
-						</div><br>
-						<div class="row" align="center">	
-							<div class="col-sm-3" align="center"></div>
-								<div class="col-sm-6" align="center">
-									<div class="card" align="center">
-										<div class="card-header" align="center">
-											<strong></strong>
-										</div>
-										<div class="card-body card-block" align="center">
-											<div id=formulario>
-												<div class="form-group">
-													<!--Incluir la ventana modal de búsqueda
-													{!!Form::open(array('url'=>'almacen/usuario/permiso/usuario','method'=>'POST','autocomplete'=>'off'))!!}
-										    		{{Form::token()}}-->	
-
-										    		{!!Form::model(0,['method'=>'PATCH','route'=>['almacen.usuario.permiso.usuario.update',0]])!!}
-    												{{Form::token()}}
-
-										<div id=formulario align="center">
-													
-													<br>
-													Cargo:
-														<select name="id_cargo" class="form-control">
-													@foreach($cargos as $car)
-													<option name="id_cargo" value="{{$car->id_cargo}}">{{$car->nombre}}</option>
-													@endforeach
-												</select>
-										<br>
-												Módulo:
-													<select name="id_modulo" class="form-control">
-													@foreach($mod as $m)
-													<option name="id_modulo" value="{{$m->id_modulo}}">{{$m->nombre}}</option>
-													@endforeach
-												</select>
-
-										<br>
-										<br>
-													<div align="center">
-													<button type="submit" class="btn btn-info">Eliminar permiso</button>
-													<a href="{{url('/')}}" class="btn btn-danger">Volver</a>
-													</div>
-												</div>
-
-										{!!Form::close()!!}	
-												</div>
-											</div>
-										</div>
-									</div>
-								</div>
-							<div class="col-sm-3" align="center"></div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
 	
 
 			
