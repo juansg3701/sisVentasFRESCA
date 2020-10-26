2b734bf23e8c2f3629181fe8a25c5aa1c74a0765
juansg3701 <54075068+juansg3701@users.noreply.github.com>
2020-10-25 20:31:47 -0500

    .

--- app/Http/Controllers/EmpleadoController.php
+++ app/Http/Controllers/EmpleadoController.php
@@ -155,6 +155,10 @@ class EmpleadoController extends Controller
 				 		$usuario->tipo_cargo_id_cargo=$cargoR;
 				 		$usuario->sede_id_sede=$sedeR;
 				 		$usuario->codigo=$codigoR;
+				 		$usuario->direccion=$request->get('direccion');
+			 			$usuario->telefono=$request->get('telefono');
+			 			$usuario->documento=$request->get('documento');
+			 			$usuario->fecha=$request->get('fecha');
 				 		$usuario->update();
 
 				 		return back()->with('msj','Empleado actualizado');
@@ -185,6 +189,10 @@ class EmpleadoController extends Controller
 					 		$usuario->sede_id_sede=$sedeR;
 					 		$usuario->codigo=$codigoR;
 					 		$usuario->user_id_user=$us->id;
+					 		$usuario->direccion=$request->get('direccion');
+					 		$usuario->telefono=$request->get('telefono');
+					 		$usuario->documento=$request->get('documento');
+					 		$usuario->fecha=$request->get('fecha');
 					 		$usuario->update();
 				 			}else{
 				 			
@@ -195,6 +203,10 @@ class EmpleadoController extends Controller
 					 		$usuario->tipo_cargo_id_cargo=$cargoR;
 					 		$usuario->sede_id_sede=$sedeR;
 					 		$usuario->codigo=$codigoR;
+					 		$usuario->direccion=$request->get('direccion');
+					 		$usuario->telefono=$request->get('telefono');
+					 		$usuario->documento=$request->get('documento');
+					 		$usuario->fecha=$request->get('fecha');
 					 		
 
 					 		$us = User::findOrFail($usuario->user_id_user);
