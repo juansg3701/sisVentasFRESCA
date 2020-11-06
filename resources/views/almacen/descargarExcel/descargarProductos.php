84583fa370515bba14a13d842ee833e256f07f56
holman-rd <63613297+holman-rd@users.noreply.github.com>
2020-11-06 16:38:10 -0500

    bajar excel

--- resources/views/almacen/descargarExcel/descargarProductos.php
+++ resources/views/almacen/descargarExcel/descargarProductos.php
@@ -5,7 +5,7 @@
 	require_once('conexion.php');
 	$conn=new Conexion();
 	$link = $conn->conectarse();
-	$query="SELECT p.id_producto, p.plu, p.ean, p.nombre, c.nombre as categoria_id_categoria, p.unidad_de_medida, i.nombre as impuestos_id_impuestos, d.nombre as descuento_id_descuento, p.stock_minimo, p.necesita_peso, p.imagen, p.precio_1, p.precio_2, p.precio_3, p.precio_4, p.costo_compra, pv.nombre as punto_venta_id_punto_venta FROM producto as p, categoria_productos as c, impuestos as i, descuento as d, punto_venta as pv WHERE p.categoria_id_categoria=c.id_categoria and p.impuestos_id_impuestos=i.id_impuestos and p.descuento_id_descuento=d.id_descuento and p.punto_venta_id_punto_venta=pv.id_punto_venta ORDER BY id_producto DESC";
+	$query="SELECT p.id_producto, p.plu, p.ean, p.nombre, c.nombre as categoria_id_categoria, p.unidad_de_medida, i.nombre as impuestos_id_impuestos, d.nombre as descuento_id_descuento, p.stock_minimo, p.necesita_peso, p.imagen, p.precio_1, p.precio_2, p.precio_3, p.precio_4, p.costo_compra, pv.no as punto_venta_id_punto_venta FROM producto as p, categoria_productos as c, impuestos as i, descuento as d, punto_venta as pv WHERE p.categoria_id_categoria=c.id_categoria and p.impuestos_id_impuestos=i.id_impuestos and p.descuento_id_descuento=d.id_descuento and p.punto_venta_id_punto_venta=pv.id_punto_venta ORDER BY id_producto DESC";
 	$result=mysqli_query($link, $query);
 ?>
 <!--Definir los campos de la tabla proveedor a mostrar en el archivo excel-->
@@ -21,7 +21,7 @@
 			<th>IMPUESTO</th>
 			<th>DESCUENTO</th>
 			<th>STOCK MINIMO</th>
-			<th>¿NECESITA PESO?</th>
+			<th>NECESIDAD - PESO</th>
 			<th>IMAGEN</th>
 			<th>PRECIO 1</th>
 			<th>PRECIO 2</th>
@@ -45,7 +45,17 @@
 				<td><?php echo $row['impuestos_id_impuestos']; ?></td>
 				<td><?php echo $row['descuento_id_descuento']; ?></td>
 				<td><?php echo $row['stock_minimo']; ?></td>
-				<td><?php echo $row['necesita_peso']; ?></td>
+
+				<td>
+					<?php
+
+						if ($row['necesita_peso'] == 1) {
+						    echo "Si";
+						}else {
+						    echo "No";
+						}
+					?>
+				</td>
 				<td><?php echo $row['imagen']; ?></td>
 				<td><?php echo $row['precio_1']; ?></td>
 				<td><?php echo $row['precio_2']; ?></td>
