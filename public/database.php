70ba833584a48cc3a0a33eccfcc6e059123a3f08
holman-rd <63613297+holman-rd@users.noreply.github.com>
2020-11-23 20:06:20 -0500

    cargar excel

--- public/database.php
+++ public/database.php
@@ -1,6 +1,6 @@
 <?php
 // parametros: host,user,password,database
-//$con = new mysqli("remotemysql.com","FzhnWzfWhw","4lwAqoEP8s","FzhnWzfWhw");
-$con = new mysqli("localhost","root","","fzhnwzfwhw");
+$con = new mysqli("remotemysql.com","FzhnWzfWhw","4lwAqoEP8s","FzhnWzfWhw");
+//$con = new mysqli("localhost","root","","fzhnwzfwhw");
 
 ?>
