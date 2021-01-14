3341ae87e917d7e5977e7b991c74cb66ea8307b5
holman-rd <63613297+holman-rd@users.noreply.github.com>
2021-01-13 23:18:35 -0500

    .

--- public/importProducto.php
+++ public/importProducto.php
@@ -144,11 +144,11 @@ if(isset($_FILES["name"])){
 
                         if($count_imp!=0 && $count_cat!=0 && $count_des!=0){
 
-                            if ($count_ean!=0 && $count_ean<=1 && $count_plu>=0 && $count_plu<=1) {
+                            if ($count_ean!=0  && $count_plu!=0 ) {
 
                                 $sql = "UPDATE producto SET nombre=\"$x_nombre\", categoria_id_categoria=\"$categoria_i\", unidad_de_medida=\"$x_unidad_de_medida\", impuestos_id_impuestos=\"$impuesto_i\", descuento_id_descuento=\"$descuento_i\", stock_minimo=\"$x_stock_minimo\", precio_1=\"$x_precio_1\", precio_2=\"$x_precio_2\", precio_3=\"$x_precio_3\", precio_4=\"$x_precio_4\", costo_compra=\"$x_costo_compra\", punto_venta_id_punto_venta=\"$x_punto_venta_id_punto_venta\", empleado_id_empleado=\"$id\", fecha_registro=\"$fecha_actual\" WHERE id_producto = \"$x_id_producto\"";
                             }else{
-                                echo '<script language="javascript">alert("----EAN y PLU deben ser valores únicos, no se guardarán cambios en el registro con el id: '.$x_id_producto.'");</script>';
+                                echo '<script language="javascript">alert("----Hay registros con el ID repetido, no se guardarán cambios en el registro con el id: '.$x_id_producto.' y nombre: '.$x_nombre.'");</script>';
                             }
 
                         }else{
